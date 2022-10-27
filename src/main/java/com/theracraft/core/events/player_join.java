c21fd56c1af946a98b7bf27fce415fec65e8aa41
claegneander <claegneander@gmail.com>
2022-10-27 13:57:20 -0500

    Added a getRank method.

--- src/main/java/com/theracraft/core/events/player_join.java
+++ src/main/java/com/theracraft/core/events/player_join.java
@@ -1,13 +1,16 @@
 package com.theracraft.core.events;
 
+import com.theracraft.core.data.rank_manager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class player_join implements Listener {
 
+    rank_manager rm = new rank_manager();
+
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event){
-
+        rm.getRank(event.getPlayer());
     }
 }
