5a84b9f0e130d0ada0e7f74bfa1073fb2e8b66a9
claegneander <claegneander@gmail.com>
2022-10-28 11:01:32 -0500

    Updated getRank; added removeRank

--- src/main/java/com/theracraft/core/data/rank_manager.java
+++ src/main/java/com/theracraft/core/data/rank_manager.java
@@ -1,58 +1,77 @@
 package com.theracraft.core.data;
 
 import com.theracraft.core.Main;
-import org.bukkit.Material;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
+
+import java.util.Objects;
 
 public class rank_manager {
 
     setup s = Main.getInstance().getSetup();
     data_handler dh = new data_handler();
 
-    public rank_manager(){
+    public rank_manager() {
 
     }
+
     public String getRank(Player player) {
         String rank = s.getRanks().get(0);
-        if (dh.hasPDCInteger(player, "rank")) {
+        if (dh.hasPDCString(player, "rank")) {
             rank = dh.getPDCString(player, "rank");
         } else {
-            for(int i = 0; i < s.getRanks().size(); i++){
-                if(player.hasPermission("theracraft.ranks." + s.getRanks().get(i))){
+            for (int i = 0; i < s.getRanks().size(); i++) {
+                if (player.hasPermission("theracraft.ranks." + s.getRanks().get(i))) {
                     rank = s.getRanks().get(i);
-                    removeOldRankData(player);
-                    setRank(player, rank);
-                    updateRank(player);
                 }
             }
         }
+        Main.getInstance().getServer().getConsoleSender().sendMessage(ChatColor.GREEN + rank);
         return rank;
     }
-    public void setRank(Player player, String rank){
+
+    public void setRank(Player player, String rank) {
         dh.setPDCString(player, "rank", rank);
+        Main.getInstance().getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "" + player.getName() + " has been given the rank: " + rank + "!");
     }
-    //Used to set the initial values of someone's rank; do not call if they are the already were setup or you will erase progress.
-    public void updateRank(Player player){
+
+    public void updateRank(Player player) {
         String rank = getRank(player);
+        setRank(player, rank);
         String display = s.getRankDisplayNames().get(rank);
         long time = s.getRankPlayTimeRequirements().get(rank);
 
         dh.setPDCString(player, "rank_display", display);
         dh.setPDCLong(player, "rank_time_requirement", time);
 
-        for(Material key: s.getRankMaterialRequirements().get(rank).keySet()){
-            int value = s.getRankMaterialRequirements().get(rank).get(key);
-            dh.setPDCInteger(player, String.valueOf(key), value);
-            System.out.println("Set PDC data for " + player.getName() + " with the key " + key.toString() + " and value of " + value);
+    }
+
+    public void removeRank(Player player) {
+        String rank = getRank(player);
+        String display = s.getRankDisplayNames().get(rank);
+        long time = s.getRankPlayTimeRequirements().get(rank);
+        if (dh.hasPDCString(player, "rank")) {
+            if (Objects.equals(dh.getPDCString(player, "rank"), rank)) {
+                dh.setPDCString(player, "rank", null);
+            } else {
+                Main.getInstance().getServer().getConsoleSender().sendMessage(ChatColor.DARK_RED + "Error code: R001. Data mismatch.");
+            }
         }
-    }
-    public void removeOldRankData(Player player){
-        //Does the opposite of updateRank by getting rid of the old material data and such.
-    }
-    //Method for confirming they have all the requirements to rank up.
-    public boolean checkRankUpStatus(Player player){
-        return true;
+        if (dh.hasPDCString(player, "rank_display")) {
+            if (Objects.equals(dh.getPDCString(player, "rank_display"), display)) {
+                dh.setPDCString(player, "rank_display", null);
+            } else {
+                Main.getInstance().getServer().getConsoleSender().sendMessage(ChatColor.DARK_RED + "Error code: R001. Data mismatch.");
+            }
+        }
+        if (dh.hasPDCLong(player, "rank_time_requirement")) {
+            if (dh.getPDCLong(player, "rank_time_requirement") == time) {
+                dh.setPDCLong(player, "rank_time_requirement", null);
+            } else {
+                Main.getInstance().getServer().getConsoleSender().sendMessage(ChatColor.DARK_RED + "Error code: R001. Data mismatch.");
+            }
+        }
+        //Need to also add materials for the rank they are on.
     }
 }
-//A way for us to calculate ticks to time values.
-//https://mapmaking.fr/tick/
+
